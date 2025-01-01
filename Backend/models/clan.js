35169417a08342eec6dfb367831f3230a0e11605
Jash_11 <agichajash@gmail.com>
2025-01-01 14:44:34 +0530

    remove models

--- Backend/models/clan.js
+++
@@ -1,9 +0,0 @@
-// backend/models/Clan.js
-const mongoose = require("mongoose");
-
-const ClanSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  points: { type: Number, required: true }
-});
-
-module.exports = mongoose.model("Clan", ClanSchema);
